85a0735d7d28e7fadb903887c7f1fd7b684f8191
이은선 <128692103+eunsunddni@users.noreply.github.com>
2026-06-15 09:42:18 +0900

    Delete Book_Creator_Platform_v2/aivle_miniproject_v2-main directory

--- Book_Creator_Platform_v2/aivle_miniproject_v2-main/backend/src/main/java/com/aivle/backend/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.aivle.backend.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long id) {
-        super("도서를 찾을 수 없습니다: " + id);
-    }
-}
